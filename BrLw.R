3f5dd896ded2ec2815ddab7a7ad2b26fcaa35640
Tracy_Alset <talset@avc.edu>
2022-07-07 22:00:33 +0000

    function modified

--- BrLw.R
+++ BrLw.R
@@ -1,11 +1,8 @@
-BrLw <-function(numClass,odds,countoDo){
+BrLw <-function(numClass=35,odds=2,countoDo=637){
 
 days=1:365
-count=countoDo
-repeatn=numClass
-varstor=numeric()
   brownlow = 0
-  for (i in 1:countoDo) {
+    for (i in 1:countoDo) {
     sam=sample(days,size=numClass,replace = T)
     y=duplicated(sam)
     if (sum(y)>0) brownlow=brownlow+1
@@ -13,3 +10,5 @@ varstor=numeric()
   }
  return(brownlow)
 }
+
+
